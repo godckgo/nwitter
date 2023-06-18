5a6eaa44e9974b31394c48725793a63aee711292
Yunz <maximum_2@icloud.com>
2023-06-18 23:27:25 +0900

    Authentification 추가

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import AppRouter from "components/Router";
+import React, { useState } from "react";
+import { authService } from "fbase"
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
